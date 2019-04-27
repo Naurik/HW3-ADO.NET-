0fd616466c35c80327b652d9c0686679b733b263
Nauryzbai <nauzik@mail.ru>
2019-04-27 16:26:51 +0600

    HW3(Ado.NET)

---
+++ HWP3/Form2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HWP3
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            
+
+        }
+
+        private static string GetConnectionString()
+        {
+            // To avoid storing the connection string in your code,
+            // you can retrieve it from a configuration file. 
+            return "Server=(local);Integrated Security=SSPI;" +
+                "Initial Catalog=AdventureWorks";
+        }
+        
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnectionStringBuilder builder =
+                                new SqlConnectionStringBuilder(GetConnectionString());
+
+            string login = textBox1.Text;
+            string password = textBox2.Text;
+
+            while (textBox1.Text != "nauryzbai" && textBox2.Text!= "12345")
+            {
+                MessageBox.Show("Неверно, введите заново");
+                return;
+            }
+            if(textBox1.Text == "nauryzbai" && textBox2.Text == "12345")
+            {
+                MessageBox.Show("Вы подключились усешно!");
+                builder.ConnectionString = $"Data Source = ACER\\SQLEXPRESS;User ID = {textBox1.Text};" +
+                $"Password = {textBox2.Text};Initial Catalog = HW3;";
+            }
+            
+        }
+
+        
+    }
+}
---
+++ HWP3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("HWP3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SSK Soft")]
+[assembly: AssemblyProduct("HWP3")]
+[assembly: AssemblyCopyright("Copyright © SSK Soft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("08b055a2-b5dd-4a9f-86d1-06807b7e7c60")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
